9a795f31a1f3066ae1e0977ebff96d7fbfd79141
Rubén Medellín <ruben.medellin.c@gmail.com>
2010-11-19 23:17:32 -0600

    Primera versión del tutorial de en marcha con Ruby on Rails

---
+++ 01_en_marcha_con_ruby_on_rails/cookbook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '54ada22684763ecf7be441597e9647403248d9ccc3ae88fb1dee104b813473af056c80f2ff7d9b0bda50b2fbc57ad87d84f780bf15bebdfe9636eaf32f6597ff'
